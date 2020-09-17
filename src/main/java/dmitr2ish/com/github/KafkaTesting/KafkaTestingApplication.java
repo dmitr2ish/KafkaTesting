888e6bd0326c511df7062521c77a01aa664834c5
dishmitov <dishmitov@appliedtech.ru>
2020-09-17 19:37:06 +0500

    Initial commit

---
+++ src/main/java/dmitr2ish/com/github/KafkaTesting/KafkaTestingApplication.java
@@ -0,0 +1,13 @@
+package dmitr2ish.com.github.KafkaTesting;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class KafkaTestingApplication {
+
+	public static void main(String[] args) {
+		SpringApplication.run(KafkaTestingApplication.class, args);
+	}
+
+}
---
+++ src/test/java/dmitr2ish/com/github/KafkaTesting/KafkaTestingApplicationTests.java
@@ -0,0 +1,13 @@
+package dmitr2ish.com.github.KafkaTesting;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class KafkaTestingApplicationTests {
+
+	@Test
+	void contextLoads() {
+	}
+
+}
