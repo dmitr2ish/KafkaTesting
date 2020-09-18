b832db322122cbacf478aeec1961767944d3ced1
dishmitov <dishmitov@appliedtech.ru>
2020-09-18 10:50:35 +0500

    - created consumer

--- src/main/java/dmitr2ish/com/github/KafkaTesting/KafkaTestingApplication.java
+++ src/main/java/dmitr2ish/com/github/KafkaTesting/KafkaTestingApplication.java
@@ -2,9 +2,17 @@ package dmitr2ish.com.github.KafkaTesting;
 
 import org.springframework.boot.SpringApplication;
 import org.springframework.boot.autoconfigure.SpringBootApplication;
+import org.springframework.kafka.annotation.EnableKafka;
+import org.springframework.kafka.annotation.KafkaListener;
 
+@EnableKafka
 @SpringBootApplication
 public class KafkaTestingApplication {
+
+	@KafkaListener(topics="msg")
+	public void msgListener(String msg) {
+		System.out.println(msg);
+	}
 
 	public static void main(String[] args) {
 		SpringApplication.run(KafkaTestingApplication.class, args);
