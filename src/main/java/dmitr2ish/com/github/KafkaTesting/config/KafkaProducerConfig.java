57b3f56cbad32308b83b59d12627e7c83e85fb3f
dishmitov <dishmitov@appliedtech.ru>
2020-09-18 11:31:38 +0500

    - added producer config and callback producer;

---
+++ src/main/java/dmitr2ish/com/github/KafkaTesting/config/KafkaProducerConfig.java
@@ -0,0 +1,46 @@
+package dmitr2ish.com.github.KafkaTesting.config;
+
+import org.apache.kafka.clients.producer.Producer;
+import org.apache.kafka.clients.producer.ProducerConfig;
+import org.apache.kafka.common.serialization.LongSerializer;
+import org.apache.kafka.common.serialization.StringSerializer;
+import org.springframework.context.annotation.Bean;
+import org.springframework.context.annotation.Configuration;
+import org.springframework.kafka.core.DefaultKafkaProducerFactory;
+import org.springframework.kafka.core.KafkaTemplate;
+import org.springframework.kafka.core.ProducerFactory;
+
+import java.util.HashMap;
+import java.util.Map;
+
+//Producer Configuration
+@Configuration
+public class KafkaProducerConfig {
+
+    final private String kafkaServer = "localhost:9092";
+
+    @Bean
+    public Map<String, Object> producerConfigs() {
+        Map<String, Object> props = new HashMap<>();
+        //added server address
+        props.put(ProducerConfig.BOOTSTRAP_SERVERS_CONFIG,
+                kafkaServer);
+        //added for long key uses
+        props.put(ProducerConfig.KEY_SERIALIZER_CLASS_CONFIG,
+                LongSerializer.class);
+        //added to use an object as a value
+        props.put(ProducerConfig.VALUE_SERIALIZER_CLASS_CONFIG,
+                StringSerializer.class);
+        return props;
+    }
+
+    @Bean
+    public ProducerFactory<Long, String> producerFactory() {
+        return new DefaultKafkaProducerFactory<>(producerConfigs());
+    }
+
+    @Bean
+    public KafkaTemplate<Long, String> kafkaTemplate() {
+        return new KafkaTemplate<>(producerFactory());
+    }
+}
--- src/main/java/dmitr2ish/com/github/KafkaTesting/controller/rest/MsgController.java
+++ src/main/java/dmitr2ish/com/github/KafkaTesting/controller/rest/MsgController.java
@@ -2,6 +2,8 @@ package dmitr2ish.com.github.KafkaTesting.controller.rest;
 
 import org.springframework.beans.factory.annotation.Autowired;
 import org.springframework.kafka.core.KafkaTemplate;
+import org.springframework.kafka.support.SendResult;
+import org.springframework.util.concurrent.ListenableFuture;
 import org.springframework.web.bind.annotation.PostMapping;
 import org.springframework.web.bind.annotation.RequestMapping;
 import org.springframework.web.bind.annotation.RestController;
@@ -10,16 +12,20 @@ import org.springframework.web.bind.annotation.RestController;
 @RequestMapping("/msg")
 public class MsgController {
 
-    final private KafkaTemplate<String, String> kafkaTemplate;
+    final private KafkaTemplate<Long, String> kafkaTemplate;
 
     @Autowired
-    public MsgController(KafkaTemplate<String, String> kafkaTemplate) {
+    public MsgController(KafkaTemplate<Long, String> kafkaTemplate) {
         this.kafkaTemplate = kafkaTemplate;
     }
 
     //this is the producer, he send message with "msg" topic, if this topic wasn't, it will be create automatically
     @PostMapping
-    public void sendOrder(String msgId, String msg){
-        kafkaTemplate.send("msg", msgId, msg);
+    public void sendOrder(Long msgId, String msg) {
+        //it need for view the result of sending a message
+        ListenableFuture<SendResult<Long, String>> future = kafkaTemplate.send("msg", msgId, msg);
+        //addCalback have two parameters - SuccesCallback nad FailureCallback
+        future.addCallback(System.out::println, System.err::println);
+        kafkaTemplate.flush();
     }
 }
